bfccca3064837015058a8fdc6ce72ec160df56dd
f-s-plus-plus <fsaulean@uvm.edu>
2018-11-01 22:51:26 -0400

    true version

---
+++ my-app/build/precache-manifest.8717d9242e8136ce2289d236d4fdeb8e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d143525e96aa678b2680ed85495050e",
+    "url": "/static/media/profile-pic.9d143525.png"
+  },
+  {
+    "revision": "938c6e20c6b4fa70a8dc",
+    "url": "/static/css/main.aaaa827a.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dfc01206d1e695e8caf6",
+    "url": "/static/js/1.dfc01206.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b2e8d7445269a0b5c17ebb99a96c2d6",
+    "url": "/static/media/resume.4b2e8d74.docx"
+  },
+  {
+    "revision": "5faa0c05f9464d23657ced77105117a1",
+    "url": "/static/media/Gidole-Regular.5faa0c05.otf"
+  },
+  {
+    "revision": "938c6e20c6b4fa70a8dc",
+    "url": "/static/js/main.938c6e20.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "dfc01206d1e695e8caf6",
+    "url": "/static/css/1.f7ecf54c.chunk.css"
+  },
+  {
+    "revision": "3fabae55be88d60e14ee827ef44750cc",
+    "url": "/index.html"
+  }
+];
