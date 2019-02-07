a712ab001851213628c5d2806c4e95fffa0583c7
f-s-plus-plus <fsaulean@uvm.edu>
2019-02-07 17:45:46 -0500

    changed profile pic

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -3,15 +3,10 @@ import React from 'react';
 //for the profile icon and description
 class Profile extends React.Component {
 
-    //link for the profile icon
-    sendLink = () => {
-        window.location.href = "https://www.britannica.com/biography/Mithradates-VI-Eupator";
-    };
-
     render() {
         return(
         <div className="profile-box">
-            <div className="profile-pic" onClick={this.sendLink} />
+            <div className="profile-pic" />
             <p className='underline-text'>
                 Who Am I?
             </p>
